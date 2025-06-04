7e75f86ea722a249f85b06499f609353a4cd4ea9
amanshresthaa <159779640+amanshresthaa@users.noreply.github.com>
2025-06-04 20:45:11 +0100

    Update

--- types/next.d.ts
+++ types/next.d.ts
@@ -2,8 +2,8 @@ import type { NextPage, NextPageContext, NextComponentType } from 'next';
 
 declare module 'next' {
   export type PageProps = {
-    params: Promise<{ [key: string]: string | string[] }>;
-    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+    params: { [key: string]: string | string[] };
+    searchParams?: { [key: string]: string | string[] | undefined };
   }
 
   export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
